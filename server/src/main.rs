5cb27fb6fe5c57b02615c8c7699d70b4953756c1
Caznix <caznix01@gmail.com>
2024-12-13 14:41:50 -0500

    test rocket get package route

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,38 @@
 use std::{env, time::Duration};
 
-use rocket::{http, serde::{Deserialize, Serialize}};
 use anyhow::{anyhow, Ok, Result};
-use log::{
-    info,
-    warn,
-    debug,
-    error,
+use log::{debug, error, info, warn};
+use rocket::{
+    http,
+    serde::{Deserialize, Serialize},
 };
 pub mod workspace;
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 #[get("/get/<package>/<version>")]
-fn give_stuff(package: &str, version: &str) -> http::Status {
+fn give_stuff(package: &str, version: &str) -> String {
     info!("requested package name: '{package}' requested package version: '{version}'");
-    return rocket::http::Status { code: 400u16 }
+    let response = format!("{package}V{version}");
+    response
 }
 
-
 async fn run_server() -> Result<()> {
-    let _server= rocket::build()
-        .mount("/", routes![index,give_stuff])
+    let _server = rocket::build()
+        .mount("/", routes![index, give_stuff])
         //.register("/", )
         .launch()
-
-        .await.map_err(|e| {
+        .await
+        .map_err(|e| {
             error!("An error occured");
             anyhow!(format!("Failed to start rocket server: {e:#}"))
         });
     Ok(())
 }
-
 
 async fn test_stuff() -> Result<()> {
     tokio::time::sleep(Duration::new(5, 0)).await;
@@ -47,12 +45,8 @@ async fn main() -> Result<()> {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
     info!("Server started");
-    let (a,b) = tokio::join!(
-        test_stuff(),
-        run_server(),
-    );
+    let (a, b) = tokio::join!(test_stuff(), run_server(),);
 
-    
     //run_server().await?;
 
     Ok(())
